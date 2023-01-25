546b8402e3c7eeecf594eb6f3511d656b6d4357f
Melnikov Ruslan <Melnikovruslan17@yandex.ru>
2023-01-25 22:56:36 +0300

    changed class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-       Server server = new Server();
-       server.start();
+        final var server = new Server();
+
+        server.addHandle("GET", "/events.html", (request, responseStream) -> {
+            Request.info(request);
+            try {
+                Server.success200(request, responseStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.listen(9999);
     }
 }
